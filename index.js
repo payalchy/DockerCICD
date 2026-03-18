6dbd3e9a8c1af6ae29a9e7aab922a4d88524da8e
payalchy <payalchy101@gmail.com>
2026-03-18 07:31:13 +0545

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Welcome World" }');
+    res.send('{ "response": "Welcome to  World" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
